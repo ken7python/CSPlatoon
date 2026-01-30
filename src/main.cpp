3c51a4a4ace28ed47f8466b6cfb6451e03c1741c
ken7python <active.ken.315@gmail.com>
2026-01-30 21:04:20 +0900

    Coltroller and Filling.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 // open build/CSplatoon.app
 #include "raylib.h"
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
@@ -17,13 +19,12 @@ private:
     int speedX;
     int speedY;
     float radius;
+    Color color;
+    Color fillColor;
 
 public:
-    Color color;
     void Draw() {
-        int posX = static_cast<int>(x);
-        int posY = static_cast<int>(y);
-        DrawCircle(posX, posY, radius, color);
+        DrawCircle(GetX(), GetY(), radius, color);
     }
 
     void moveRight(float a = 1.0f) {
@@ -61,75 +62,123 @@ public:
         }
     }
 
-    Player(float posx, float posy, Color c) {
+    int GetX() {
+        return static_cast<int>(x);
+    }
+    int GetY() {
+        return static_cast<int>(y);
+    }
+    Vector2 GetVector() {
+        return Vector2{ x, y };
+    }
+    float GetRadius() {
+        return radius;
+    }
+
+    Color GetFillColor() {
+        return fillColor;
+    }
+
+    Player(float posx, float posy, Color c,Color fc = WHITE) {
         x = posx;
         y = posy;
         speedX = 640.0f;
         speedY = 640.0f;
         color = c;
+        fillColor = fc;
         radius = 48.0f;
     }
-
-
 };
 
-const int dotSize = 5;
+const float dotSize = 3.0f;
 class Dot {
 private:
-    int x;
-    int y;
-    int width;
-    int height;
+    float x;
+    float y;
+    float width;
+    float height;
     Color color;
+    Rectangle dot;
+
+    bool isHit(Player* p) {
+        Vector2 playerPos = p->GetVector();
+        float playerRadius = p->GetRadius();
+        return CheckCollisionCircleRec(playerPos, playerRadius, dot);
+    }
 public:
     Dot() : Dot(0, 0) {}
-    Dot(int posX, int posY, Color c = LIGHTGRAY) {
+    Dot(float posX, float posY, Color c = LIGHTGRAY) {
         x = posX;
         y = posY;
         width = dotSize;
         height = dotSize;
+        dot = Rectangle{ x, y, width, height };
         color = c;
     }
     void Draw() {
-        DrawRectangle(x, y,width, height, color);
+        DrawRectangleRec(dot, color);
+    }
+    void Col(Player* p) {
+        if (isHit(p)) {
+            color = p->GetFillColor();
+        }
     }
 };
+
+void padColtrol(Player* p ,int n) {
+    float PadX = 0;
+    float PadY = 0;
+    if (IsGamepadAvailable(n)) {
+        PadX = GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_X);
+        PadY = GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_Y);
+        // cout << "P1 Pad X: " << p1PadX << " Y: " << p1PadY << endl;
+        p->moveX(PadX > 0.2f || PadX < -0.2f ? PadX : 0);
+        p->moveY(PadY > 0.2f || PadY < -0.2f ? PadY : 0);
+
+        if (IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_LEFT)) {
+            p->moveLeft(1.0f);
+        }
+        if (IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_RIGHT)) {
+            p->moveRight(1.0f);
+        }
+        if (IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_UP)) {
+            p->moveUp(1.0f);
+        }
+        if (IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_DOWN)) {
+            p->moveDown(1.0f);
+        }
+    }
+}
 
 int main() {
     InitWindow(screenWidth, screenHeight, "CSplatoon");
     SetTargetFPS(60);
 
-    Player player1(100, 100, BLUE);
+    Player player1(100, 100, BLUE, SKYBLUE);
 
-    float p1PadX = 0;
-    float p1PadY = 0;
 
     int H = 0;
     int W = 0;
-    const int numH = screenHeight / dotSize;
-    const int numW = screenWidth / dotSize;
-    Dot dots[numH][numW];
+    const int numH = static_cast<int>(ceil(screenHeight / dotSize) );
+    const int numW = static_cast<int>(ceil(screenWidth / dotSize) );
+    vector<vector<Dot>> dots(numH, vector<Dot>(numW));
     cout << "numH: " << numH << " numW: " << numW << endl;
     while (H < numH) {
         W = 0;
         while (W < numW) {
-            dots[H][W] = Dot(W * dotSize, H * dotSize);
+            dots[H][W] = Dot(static_cast<float>(W) * dotSize, static_cast<float>(H) * dotSize);
             W++;
         }
         H++;
     }
     cout << "Dots initialized." << endl;
 
+    float nowFPS = 0.0f;
+
     while (!WindowShouldClose()) {
         dt = GetFrameTime();
-        if (IsGamepadAvailable(0)) {
-            p1PadX = GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_X);
-            p1PadY = GetGamepadAxisMovement(0, GAMEPAD_AXIS_LEFT_Y);
-            // cout << "P1 Pad X: " << p1PadX << " Y: " << p1PadY << endl;
-            player1.moveX(p1PadX > 0.2f || p1PadX < -0.2f ? p1PadX : 0);
-            player1.moveY(p1PadY > 0.2f || p1PadY < -0.2f ? p1PadY : 0);
-        }
 
+        padColtrol(&player1, 0);
         if (IsKeyDown(KEY_A)) {
             player1.moveLeft();
         }
@@ -150,6 +199,7 @@ int main() {
             while (H < numH) {
                 W = 0;
                 while (W < numW) {
+                    dots[H][W].Col(&player1);
                     dots[H][W].Draw();
                     W++;
                 }
@@ -158,6 +208,9 @@ int main() {
 
             player1.Draw();
             // DrawText("Hello Raylib!", 280, 200, 20, DARKGRAY);
+        nowFPS = GetFPS();
+        DrawText(TextFormat("FPS: %.2f", nowFPS), 10, 10, 20, BLACK);
+
         EndDrawing();
     }
 
